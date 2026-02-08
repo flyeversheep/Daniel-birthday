c1a3747a42fdfa84747cf5a518285c2970ddff38
apple <feihan@appledeMacBook-Pro.local>
2026-02-07 16:51:58 -0800

    Add PAW Patrol images to webpage

---
+++ copy-images.sh
@@ -0,0 +1,3 @@
+# Copy PAW Patrol images to project folder
+cp "/Users/feihan/.clawdbot/media/inbound/868182d4-d04e-43e2-b74b-75f1a6a93543.jpg" "pawpatrol1.jpg"
+cp "/Users/feihan/.clawdbot/media/inbound/37452ae5-265b-46e6-a86c-62517872570c.jpg" "pawpatrol2.jpg"
